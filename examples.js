de47bb0e05b39f71b10d1f949e6419f6cf156d90
Melissa Steffan <msteffan2012@gmail.com>
2016-09-29 22:55:37 -0400

    start working on jquery examples and lesson code

--- examples.js
+++ examples.js
@@ -1,204 +1,98 @@
 /*
 
-JAVASCRIPT BUILDING BLOCKS
+Week 7, Seminar 2: the DOM & jQuery
 
-What is JavaScript?
-    - "client-side scripting language"
-
-    - Scripting language = no compilation required
-
-    - easy to get started -> just make a .js file
-
-    - put .js files right before </body> tag
+Goals:
+  - Understand the DOM
+  - Understand what jQuery is
+  - Understand jQuery syntax
+  - Understand how to modify HTML with jQuery
 
 */
-
-
-
-
-
-// Variables
-  // a way to store information
-
-// declaration               // value
-          var pizzaTopping = "cheese"
-                // name
-
-                       // expression
-          var twenty = 5 * 4
-                       // value
-          var twenty = 20
-
-
-
-
-
-
-
-// a number is a number!
-var number = 5;
-
-// a string is text
-var string = "hello";
-var string2 = "1";
-var string3 = "Hello " + 1; // Hello 1
-var string4 = "1" + 1; // 11
-// a number in quotes (i.e. "5") is read like text; a number without quotes is read like a number
-
-// a boolean is either true or false
-var boolean = true;
-var boolean = false;
-
-
-
-// An Object
-// Like a dictionary (one definition for every word)
-var demoObject = {
-  key1:"value",
-  key2:"value",
-  key3:"value"
-}
-
-var favoriteMeals = {
-  breakfast: "Waffles with peanut butter",
-  lunch: "Turkey and havarti sandwich",
-  dinner: "Pizza",
-  mealsADay: 3
-}
-
-// How do we access something in an object?
-// "dot notation"
-  favoriteMeals.dinner
-
-// "bracket notation"
-  favoriteMeals["lunch"]
 
 /*
-A BRIEF INTERLUDE FOR AN IMPORTANT MESSAGE!
 
-- JavaScript is case-sensitve
-  - favoriteMeals does not equal favoritemeals
-  - favoriteMeals.Dinner isn't favoriteMeals.dinner
 
-- Best practice = camel-casing
-  - thisIsAnExampleOfCamelCasing
-  - firstWordLowerCased
-  - thenOtherWordsCapitalized
+What is the DOM?
+
+  - Stands for "document object model"
+
+  - Standard way for browsers to represent HTML to JavaScript
+
+  - Basically: a clone of your HTML
+
+
+
 
 */
 
 
-// An Array
-// like a numbered list -- every item has a number
-  // An item's number is called an "index"
-  // indexes start at 0 & count up
-var demoArray = ["blue", 1, "2", "pineapples", false]
-
-var fruits = ["apples", "oranges", "bananas", "pineapples"]
-
-// How do we access something in an array?
-fruits[0]
-fruits[4]
-fruits[5] // doesn't exist! :(
+/*
 
 
-// Control Flow
-  // oh man, scary words!!
-  // not scary version of the words: "how we want the computer to decide things"
 
-// if statement
-if ( an email address is present ){
+What is jQuery?
 
-}
 
-// if-else statement
-if ( email and password match ){
- DO SOMETHING
+  - JavaScript library that lets us "do more, write less"
+
+  - "Helper" methods to simplify JavaScript
+
+  - under the hood: jQuery === JavaScript
+
+
+*/
+
+// vanilla JS
+document.getElementById("logo").style.backgroundColor = "yellow"
+
+// jQuery
+$("#logo").css("background-color", "yellow")
+
+
+
+
+// vanilla
+var el = document.getElementById("#logo");
+if (el.classList) {
+  el.classList.toggle(className);
 } else {
- DO SOMETHING ELSE
+  var classes = el.className.split(' ');
+  var existingIndex = classes.indexOf(className);
+
+  if (existingIndex >= 0)
+    classes.splice(existingIndex, 1);
+  else
+    classes.push(className);
+
+  el.className = classes.join(' ');
 }
 
-// if()-else if()-else statement
-if (  ){
 
-} else if (  ) {
-
-} else {
-
-}
-
-
-
-// FUNCTIONS
-  // Reuseable bits of code
-
-  // declaring our function:
- //declaration  //name  //parameter
-     function sayHello(firstName){
-       // function body goes here!
-       return "Hello, " + firstName + "!"
-     }
-
-// invoking our function:
-   //name  //argument
-  sayHello("Melissa")
+// ...jQuery
+$("#logo").toggleClass(className);
 
 
 
 
 
-
-// Selectors
-
-document.getElementById('id')
-
-document.getElementsByClassName('className')
-
-document.querySelector('query')
-
-
-// example:
-document.getElementById("logo")
-
-document.querySelector("#logo")
+      //selector
+      $("#logo").toggleClass(className);
+                  // action
 
 
 
 
 
-// Methods
+              // getter
+          $("#logo").css("color");
 
-// set a css property
-document.getElementById("bagel").style.color = "goldenrod";
-
-// add a CSS className
-document.querySelector('query').classList.add("anotherclass");
-
-
-// Events and event listeners
-
-  // using an anonymous function
-  document.querySelector("").addEventListener('click', function(){
-    // do cool stuff
-  })
-
-  // using a named function
-  function doCoolStuff(){
-    // do cool stuff
-  }
-
-  document.querySelector("").addEventListener('click', doCoolStuff)
+        // output: "rgb(255, 165, 0)"
 
 
 
 
+              // setter
+      $("#logo").css("color", "blue");
 
-
-
-
-
-
-
-
-
-// fin
+// output: <h1 id="logo" style="color: blue;">
